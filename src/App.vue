daf52e489906a44fb2e16523f601b3896ee6445b
Raiden Lin <rodes5292@gmail.com>
2021-01-20 15:09:43 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<script>
+import { computed, onMounted, reactive, ref } from 'vue'
+
+export default {
+
+  setup() {
+    let Player = ref()
+
+    let options = reactive({
+      techOrder: ["youtube"], 
+      sources: [
+        { 
+          "type": "video/youtube", 
+          "src": "https://www.youtube.com/watch?v=pjCyDeEYSq8"
+        }
+      ]
+    })
+
+    const optionToJSON = computed(()=> {
+      console.log(JSON.stringify(options));
+      return JSON.stringify(options) 
+    })
+
+    onMounted(()=> {
+      // console.log(Player);
+    }) 
+
+    return {
+      Player,
+      optionToJSON
+    }
+  }
+}
+</script>
+
+
+<template lang='pug'>
+video(ref='Player' id="vid1" class="video-js vjs-default-skin vjs-big-play-centered" controls autoplay width="1280" height="720" :data-setup='optionToJSON') 
+
+
 </template>
 
 <style lang="stylus">
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import videojsYoutube from 'videojs-youtube'
+import 'video.js/dist/video-js.css'
+createApp(App).use(store).use(router).use(videojsYoutube).mount('#app')
